75fbfa0ac2f9f3898a6e771aeb001478f5d5b06d
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-06-03 20:43:26 +0800

    Added __safeExecution method and changed other method way of execution

---
+++ PySql.py
@@ -0,0 +1,29 @@
+import mysql.connector
+class PySql:
+    def __init__(self, username, userPassword):
+        try:
+            self.db = mysql.connector.connect(
+                host="localhost",
+                user=username,
+                password=userPassword
+            )
+            self.cursor = self.db.cursor();
+        except:
+            print("Invalid Credentials")
+            exit(-1)
+    
+    
+    ########## Database Stuffs ##########
+    def useDatabase(self, databaseName):
+        self.__safeExecution(f"USE {databaseName}", "Using Database")
+
+    def createDatabase(self, databaseName):
+        self.__safeExecution(f"CREATE DATABASE{databaseName}", "Database Creation")
+
+        
+    def __safeExecution(self, command, typeOfCommand="Unknown"):
+        try:
+            self.cursor.execute(command)
+        except:
+            print(f"Problem doing {typeOfCommand}");
+            exit(-1)
