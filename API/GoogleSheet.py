8da5c901a9bf5063219d05a475ae418f0a343ead
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-06-13 20:25:24 +0800

    Remove unnecessary imports

--- API/GoogleSheet.py
+++ API/GoogleSheet.py
@@ -1,11 +1,9 @@
 from __future__ import print_function
-from google.oauth2 import service_account
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 import os.path
 
 
